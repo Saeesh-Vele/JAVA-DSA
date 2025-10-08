11e28e1f73d17fe9fd65974ce08f0fa4f4eaa962
Saeesh-Vele <velesaeesh@gmail.com>
2025-10-08 21:49:53 +0530

    Update DtoB.java

--- Functions and Methods/DtoB.java
+++ Functions and Methods/DtoB.java
@@ -1,8 +1,19 @@
 public class DtoB {
 
-    public static void 
+    public static void DecimalToBinary(int n){
+        int myNum = n ;
+        int pow = 0 ;
+
+        int binary = 0 ;
+        while(n>0){
+            int rem = n%2 ;
+            binary = binary + (rem * (int)Math.pow(10, pow)) ;
+            pow++ ;
+            n = n/2 ; 
+        }
+        System.out.println("Binary Form of : " + myNum + " = " + binary);
+    }
     
     public static void main(String[] args) {
-        
+        DecimalToBinary(7);
     }
-}
